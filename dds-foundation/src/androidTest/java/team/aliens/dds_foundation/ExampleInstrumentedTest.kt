9464cab77af44d9542d3c5d04d40091c16cb23ea
limsaehyun <sh00710000@gmail.com>
2023-03-27 16:30:23 +0900

    feat: dds-foundation module

---
+++ dds-foundation/src/androidTest/java/team/aliens/dds_foundation/ExampleInstrumentedTest.kt
@@ -0,0 +1,31 @@
+/*
+ * Designed and developed by Team Aliens, 2023
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/team-aliens/DDS-Android/blob/develop/LICENSE
+ */
+
+package team.aliens.dds_foundation
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("team.aliens.dds_foundation.test", appContext.packageName)
+    }
+}
