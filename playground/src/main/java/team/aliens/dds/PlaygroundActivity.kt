2460d5254cae170f7f7cea823a6bc7c40327a4bc
limsaehyun <sh00710000@gmail.com>
2023-03-27 16:32:25 +0900

    fix: delete unused code

---
+++ playground/src/main/java/team/aliens/dds/PlaygroundActivity.kt
@@ -0,0 +1,21 @@
+/*
+ * Designed and developed by Team Aliens, 2023
+ *
+ * Licensed under the MIT.
+ * Please see full license: https://github.com/team-aliens/DDS-Android/blob/develop/LICENSE
+ */
+
+package team.aliens.dds
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class PlaygroundActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            // TODO(limsaehyun): PlayGround UI 작업
+        }
+    }
+}
